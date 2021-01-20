6e7b10fd19063e81e8e6a31617baa66ec2755125
paul.hasselkuss <git@hasselkuss.com>
2021-01-20 16:27:38 +0100

    Update site

--- site/wp-content/themes/ixion/assets/js/testimonials.js
+++
@@ -1,9 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-	$( function() {
-		$( '.testimonials' ).masonry( {
-			columnWidth: '.grid-sizer',
-			gutter: '.gutter-sizer',
-			itemSelector : '.testimonials .hentry'
-		} );
-	} );
-} );
