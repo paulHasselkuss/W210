f0fdc16c90bdac7b0333a1abb418a134096699c2
paul.hasselkuss <git@hasselkuss.com>
2024-03-19 12:22:53 +0100

    Improve search markup and script

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,13 @@
 'use strict';
 import Document from 'js/vendor/flexsearch/document.js';
 
-const search_form = document.getElementById('searchForm'); // search form
-const search_input = document.getElementById('searchInput'); // input box for search
-const search_results = document.getElementById('searchListItems'); // targets the <ul>
-const search_results_title = document.getElementById('searchListTitle');
 const search_toggle = document.getElementById('searchToggle');
-const options = {
+const search_cancel = document.getElementById('searchCancel');
+const search_form = document.getElementById('searchForm');
+const search_input = document.getElementById('searchInput');
+const search_results = document.getElementById('searchResults');
+
+const flexsearch_options = {
   preset: 'match',
   context: 'true',
   tokenize: 'full',
@@ -16,15 +17,96 @@ const options = {
     store: ['title', 'href'],
   },
 };
-let index; // holds our search engine
-let search__focus = false; // check to true to make visible by default
-let results_available = false;
-let first_run = true; // allow us to delay loading json data unless search activated
-search_toggle.addEventListener('click', function (e) {
-  search_toggle_focus(e);
-});
+let flexsearch;
 
-document.addEventListener('keydown', function (e) {
+let has_focus = false;
+let has_results = false;
+let has_init_flexsearch = false;
+
+function search_toggle_focus() {
+  //console.log(e); // DEBUG
+  // order of operations is very important to keep focus where it should stay
+  if (!has_focus) {
+    document.body.classList.add('search-active');
+    search_toggle.classList.add('is-active');
+    search_input.focus(); // move focus to search box
+    has_focus = true;
+  } else {
+    document.body.classList.remove('search-active');
+    search_toggle.classList.remove('is-active');
+    document.activeElement.blur(); // remove focus from search box
+    has_focus = false;
+  }
+}
+
+function search_init() {
+  if (!has_init_flexsearch) {
+    has_init_flexsearch = true; // let's never do this again
+    fetch(search_form.getAttribute('data-base-url') + search_form.getAttribute('data-language-prefix') + '/index.json')
+      .then(data => data.json())
+      .then(data => {
+        flexsearch = new Document(flexsearch_options);
+        data.forEach(data => flexsearch.add(data));
+        search_input.addEventListener('keyup', function () { // execute search as each character is typed
+          search_exec(this.value);
+        });
+        //console.log('index.json loaded'); // DEBUG
+      });
+  }
+}
+
+function search_exec(term) {
+  has_results = false;
+  if (term.length > 3) {
+    let results = flexsearch.search(term, { enrich: true });
+    if (results.length !== 0) {
+      has_results = true;
+    }
+    print_results(term, results);
+  }
+}
+
+function print_results(term, results) {
+  let title_text = '';
+  if (results.length === 0) {
+    title_text = 'No results.'
+  } else if (results[0].result.length === 1) {
+    title_text = '1 result'
+  } else {
+    title_text = `${results[0].result.length} results`
+  }
+
+  let results_text = '';
+  let regex = new RegExp(term.split(/\s+/).filter(Boolean).join('|'), 'gi');
+  results[0].result.forEach(item => results_text += item_to_html(item, regex));
+
+  search_results.innerHTML = `
+    <h2 class="search-list__title bigtext">${title_text}</h2>
+    <ul id="searchResultsList" class="search-list__items unstyled">${results_text}</ul>
+    `;
+}
+
+function item_to_html(item, regex) {
+  let item_title = item.doc.title.replace(regex, match => `<mark>${match}</mark>`);
+  return `
+    <li class='search-result'>
+      <a class='unstyled' href='${item.doc.href}' tabindex = '0'>
+        <h3 class='search-result__title'>${item_title}</h3>
+        <p class='search-result__path'>${item.doc.href}</p>
+      </a>
+    </li>
+    `;
+}
+
+function get_first_result() {
+  return document.querySelector('#searchResults ul').firstElementChild.firstElementChild;
+}
+
+function get_last_result() {
+  return document.querySelector('#searchResults ul').lastElementChild.firstElementChild;
+}
+
+document.addEventListener('keydown', e => {
   // console.log(event); // DEBUG
   // Ctrl + / to show or hide Search
   // if (event.metaKey && event.which === 191) {
@@ -32,21 +114,21 @@ document.addEventListener('keydown', function (e) {
     search_toggle_focus(e); // toggle visibility of search box
   }
 
-  if (!search__focus) {
+  if (!has_focus) {
     return;
   }
   // Use Enter (13) to move to the first result
   if (e.keyCode == 13) {
     if (document.activeElement == search_input) {
       e.preventDefault(); // stop form from being submitted
-      if (results_available) {
+      if (has_results) {
         get_first_result().focus();
       }
     }
   }
   // DOWN (40) arrow
   if (e.keyCode == 40) {
-    if (results_available) {
+    if (has_results) {
       e.preventDefault(); // stop window from scrolling
       if (document.activeElement == search_input) {
         // if the currently focused element is the main input --> focus the first <li>
@@ -62,7 +144,7 @@ document.addEventListener('keydown', function (e) {
   }
   // UP (38) arrow
   if (e.keyCode == 38) {
-    if (results_available) {
+    if (has_results) {
       e.preventDefault(); // stop window from scrolling
       if (document.activeElement == search_input) {
         // If we're in the input box, do nothing
@@ -84,83 +166,18 @@ document.addEventListener('keydown', function (e) {
     }
   }
 });
+
+//toogle search visibility
+search_toggle.addEventListener('click', e => search_toggle_focus(e));
+search_cancel.addEventListener('click', e => search_toggle_focus(e));
+
 //init when the form is in focus
-search_form.addEventListener('focusin', function () {
-  search_init();
-});
-search_form.addEventListener('keydown', function (e) {
-  // Allow ESC (27) to close search box
+search_form.addEventListener('focusin', e => search_init(e));
+
+// Allow ESC (27) to close search box
+search_form.addEventListener('keydown', e => {
   if (e.keyCode == 27) {
-    search__focus = true; // make sure toggle removes focus
+    has_focus = false; // make sure toggle removes focus
     search_toggle_focus(e);
   }
 });
-
-function search_toggle_focus() {
-  //console.log(e); // DEBUG
-  // order of operations is very important to keep focus where it should stay
-  if (!search__focus) {
-    document.body.classList.add('search-active');
-    search_toggle.classList.add('is-active');
-    search_input.focus(); // move focus to search box
-    search__focus = true;
-  } else {
-    document.body.classList.remove('search-active');
-    search_toggle.classList.remove('is-active');
-    document.activeElement.blur(); // remove focus from search box
-    search__focus = false;
-  }
-}
-
-function search_init() {
-  if (first_run) {
-    first_run = false; // let's never do this again
-    fetch(search_form.getAttribute('data-base-url') + search_form.getAttribute('data-language-prefix') + '/index.json')
-      .then(data => data.json())
-      .then(data => {
-        index = new Document(options);
-        data.forEach(data => index.add(data));
-        search_input.addEventListener('keyup', function () { // execute search as each character is typed
-          search_exec(this.value);
-        });
-        //console.log('index.json loaded'); // DEBUG
-      });
-  }
-}
-
-function search_exec(term) {
-  let results_html = ''; // our results bucket
-  let results_title_html = ''
-  results_available = false;
-  if (term.length > 3) {
-    let results = index.search(term, { enrich: true });
-    if (results.length === 0) { // no results based on what was typed into the input box
-      results_title_html = 'No results match.';
-    } else { // build our html
-      results_available = true;
-      let regex = new RegExp(term.split(/\s+/).filter(function (i) { return i === null || i === void 0 ? void 0 : i.length; }).join('|'), 'gi');
-      results[0].result.forEach(item => { results_html += result_html(item, regex) });
-      results_title_html = maybePluralize(results[0].result.length, ' result');
-    }
-  }
-  search_results_title.innerHTML = results_title_html;
-  search_results.innerHTML = results_html;
-}
-
-function result_html(item, regex) {
-  let title = item.doc.title.replace(regex, function (match) { return '<mark>' + match + '</mark>'; });
-  return `<li class='search-result'><a class='unstyled' href='${item.doc.href}' tabindex = '0'>
-    <h2 class='search-result__title'>${title}</h2>
-    <p class='search-result__path'>${item.doc.href}</p>
-    </a></li>`;
-}
-
-function get_first_result() {
-  return search_results.firstChild.firstElementChild;
-}
-
-function get_last_result() {
-  return search_results.lastChild.firstElementChild;
-}
-const maybePluralize = (count, noun, suffix = 's') =>
-  `${count} ${noun}${count !== 1 ? suffix : ''}`;
